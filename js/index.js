8313a3d1e1547cfbb0ae6f73a734c24f56dc7c7e
Saqlain Kadiri <skadiri@tavisca.in>
2019-08-20 19:54:54 +0530

    Created tabs

---
+++ js/index.js
@@ -0,0 +1,59 @@
+body{
+	margin: 0;
+	background-color: lightgray;
+}
+a{
+	text-decoration: none;
+    color: white;
+}
+.navbar ul{
+	list-style-type: none; 
+	background-color: gray;
+	padding-right: 20px;
+	padding: 10px;
+	margin: 0;
+}
+.logo{
+	color: pink;
+	padding-right: 30px;
+}
+.navbar a{
+	font-size: 30px;
+}
+.navbar li{
+	display: inline;
+	padding-left: 20px;
+}
+.navbar a:hover {
+	color: purple;
+}
+.hidden{
+	display: none;
+}
+.hidden p{
+	text-align: center;
+}
+.dropbtn {
+  background-color: gray;
+  color: pink;
+  padding: 20px;
+  font-size: 16px;
+}
+.dropdown {
+  display: inline-block;
+  margin-left: 30px;
+}
+.dropdown-content {
+  display: none;
+}
+.dropdown-content a {
+  padding: 10px 10px;
+  background-color: gray;
+  display: block;
+}
+.dropdown-content a:hover {
+	color: purple;
+}
+.dropdown:hover .dropdown-content {
+	display: block;
+}
